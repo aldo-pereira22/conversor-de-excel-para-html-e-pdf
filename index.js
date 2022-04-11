af68d6b73b1a11752a475d118c02d6765af648f4
Aldo pereira <aldopereira.gyn@gmail.com>
2022-04-10 21:40:07 -0300

    Leitura e escrita

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 const fs = require('fs')
 
-fs.readFile("./aldo.pereira", { encoding: 'utf-8' }, (erro, data) => {
-    if (erro) {
-        console.log("Erro na leitura")
-    } else {
-        console.log(data)
-    }
-})
+modificarUsuario("teste", "teste", "teste")
+
+function modificarUsuario(nome, curso, categoria) {
+
+    fs.readFile('./usuario.json', { encoding: 'utf-8' }, (erro, dados) => {
+        if (erro) {
+            console.log("Um erro aconteceu")
+        } else {
+            // console.log(dados)
+            var conteudo = JSON.parse(dados)
+            console.log(conteudo)
+            conteudo.nome = nome
+            conteudo.curso = curso
+            conteudo.categoria = categoria
+            console.log(conteudo)
+
+            fs.writeFile('./usuario.json', JSON.stringify(conteudo), (err) => {
+                if (err) {
+
+                    console.log("Um erro aconteceu")
+                }
+            })
+        }
+    })
+}
