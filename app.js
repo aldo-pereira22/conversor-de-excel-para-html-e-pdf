8592158f6d2d767434d4c5e5ab5e7f5345f66cb4
Aldo pereira <aldopereira.gyn@gmail.com>
2022-04-12 18:40:02 -0300

    Criando a classe da Tabela

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 var Reader = require('./Reader')
 var Processor = require('./Processor')
+var Table = require('./Table')
 
 var leitor = new Reader()
 
 
 async function main() {
     var dados = await leitor.Read("./user.csv")
-    console.log(dados)
+        // console.log(dados)
     var dadosProcessados = Processor.Process(dados)
+
+    var usuarios = new Table(dadosProcessados)
+
+    console.log(usuarios.rows)
 }
 
 main()
